fca0268ce715a7f9807f43ca1d52b88348c13575
Elsch <Elsch@example.com>
2013-02-01 00:01:13 +0000

    Content Pipeline Extension integrated into a few samples
    git-svn-id: https://farseerphysics.svn.codeplex.com/svn@101466 81e6e737-5a20-4d0f-a97c-cb01297b46e0

---
+++ FarseerPhysics.Content.Pipeline/RawBodyTemplate.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using FarseerPhysics.Dynamics;
+
+namespace FarseerPhysics.ContentPipeline
+{
+  struct RawFixtureTemplate
+  {
+    public string path;
+    public string name;
+    public Matrix transformation;
+    public float density;
+    public float friction;
+    public float restitution;
+  }
+
+  struct RawBodyTemplate
+  {
+    public List<RawFixtureTemplate> fixtures;
+    public string name;
+    public float mass;
+    public BodyType bodyType;
+  }
+}
